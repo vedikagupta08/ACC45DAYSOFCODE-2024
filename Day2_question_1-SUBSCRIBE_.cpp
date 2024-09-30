eb56b5d035d2be978f9ff82f16f29626ed099252
vedikagupta08 <145323225+vedikagupta08@users.noreply.github.com>
2024-09-30 22:22:16 +0530

    Update Day2_question_1-SUBSCRIBE_.cpp

--- Day2_question_1-SUBSCRIBE_.cpp
+++ Day2_question_1-SUBSCRIBE_.cpp
@@ -9,13 +9,6 @@ int main() {
 	while(t--){
 	    double N;
 	    std::cin >> N>>X;
-	   // if(N>1){
-	       
-	   // std::cout << X*ceil(N/6) << std::endl;
-	   // }
-	   // else{
-	   //     std::cout << X << std::endl;
-	   // }
 	
 	    std::cout << X*ceil(N/6) << std::endl;
 	}
