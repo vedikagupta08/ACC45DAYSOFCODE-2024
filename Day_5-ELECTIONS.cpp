16e29e4629e834e8c37516a03baf9383093e2937
vedikagupta08 <145323225+vedikagupta08@users.noreply.github.com>
2024-10-02 22:20:12 +0530

    Add files via upload

---
+++ Day_5-ELECTIONS.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+    int XA, XB, XC;
+    cin >> XA >> XB >> XC;
+     if (XA > 50) {
+        cout << "A" << endl;
+    } else if (XB > 50) {
+        cout << "B" << endl;
+    } else if (XC > 50) {
+        cout << "C" << endl;
+    } else {
+        cout << "NOTA" << endl;
+    }
+ }
+ return 0;
+
+}
+    
+    
+    
+   
