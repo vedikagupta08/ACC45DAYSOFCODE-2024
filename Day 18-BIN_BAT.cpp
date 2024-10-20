4a9618b66967b66775aa2b2afdedf687a08739e9
vedikagupta08 <145323225+vedikagupta08@users.noreply.github.com>
2024-10-20 12:43:18 +0530

    Add files via upload

---
+++ Day 18-BIN_BAT.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    int count=0,f=0;
+	    for(int i=2;i<=a;i*=2)
+	    {
+	        count++;
+	    }
+	    f=count*b+((count-1)*c);
+	    cout<<f<<endl;
+	}
+	return 0;
+}
