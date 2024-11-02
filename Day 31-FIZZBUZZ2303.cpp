c2ce96bbdc54f9fa1a12f9ccd7018d0df586b589
vedikagupta08 <145323225+vedikagupta08@users.noreply.github.com>
2024-11-02 22:17:43 +0530

    Add files via upload

---
+++ Day 31-FIZZBUZZ2303.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+     int x;
+    cin>>t;
+    while(t--){
+        cin>>x;
+        cout<<(x*x)-x<<endl;
+        
+    }
+	return 0;
+}
