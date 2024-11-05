267bd2553a43469dc8e27403d7b787400f510534
vedikagupta08 <145323225+vedikagupta08@users.noreply.github.com>
2024-11-05 22:49:15 +0530

    Add files via upload

---
+++ Day 34-BLACKJACK.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    if(a+b<=10)
+	    cout<<-1<<endl;
+	    else
+	    cout<<21-(a+b)<<endl;
+	    
+	}
+	return 0;
+}
