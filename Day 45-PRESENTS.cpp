72b872bb7e4d2ef673a31e5947c51191fb83eb59
vedikagupta08 <145323225+vedikagupta08@users.noreply.github.com>
2024-11-16 13:19:18 +0530

    Add files via upload

---
+++ Day 45-PRESENTS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int c=n/5;
+	    cout<<(n-c)<<"\n";
+	}
+	return 0;
+}
